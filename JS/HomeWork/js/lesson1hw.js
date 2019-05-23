0d3ed697db98b42f3ab19bd87124b3c4832c7aab
MalegovDmitrii <d-malegov@mail.ru>
2019-05-23 21:15:55 +0300

    Обновление

--- JS/HomeWork/js/lesson1hw.js
+++ JS/HomeWork/js/lesson1hw.js
@@ -30,7 +30,6 @@ var n = 14.01;
 var num1 = Math.abs(10 - m);
 var num2 = Math.abs(10 - n);
 var result =  Math.min(num1, num2)
-
 if (result === num1) {
     console.log(n);
 } else {
@@ -62,7 +61,7 @@ var c = (Math.pow(cX - aX, 2) + Math.pow(cY - aY, 2) + Math.pow(cZ - aZ, 2));
 if (c === a + b || b === a + c || a === b + c) {
     console.log("Треугольник Прямоугольный");
 }
-else{
+else {
 console.log("Треугольник не прямоугольный");
 
 }
@@ -70,3 +69,55 @@ console.log("Треугольник не прямоугольный");
 console.log("Длинна строны a = " + Math.sqrt(a));
 console.log("Длинна строны b = " + Math.sqrt(b));
 console.log("Длинна строны c = " + Math.sqrt(c));
+
+
+
+console.log("Домашнее задание №2")
+
+console.log("Задача №1. Дан номер месяца (1 — январь, 2 — февраль, ...). Вывести название соответствующего времени года (зима, весна и т.д.).");
+var mountNumber = 1;
+if(mountNumber == 12 || mountNumber == 1 || mountNumber == 2){
+    console.log("Зима");
+}
+else if(mountNumber == 3 || mountNumber == 4 || mountNumber == 5){
+    console.log("Весна");
+}
+else if(mountNumber == 6 || mountNumber == 7 || mountNumber == 8){
+    console.log("Лето");
+} else {
+    console.log("Осень");
+}
+
+
+console.log("Задача №2. Единицы длины пронумерованы следующим образом: 1 — дециметр, 2 — километр, 3 — метр, 4 — миллиметр, 5 — сантиметр. Дан номер единицы длины и длина отрезка L в этих единицах (вещественное число). Вывести длину данного отрезка в метрах.");
+var indexUnits = 5;
+var iLong = 2546;
+switch(indexUnits){
+    case 1:
+    console.log(iLong + " дециметров = " + iLong / 10 + " метров");
+    break;
+    case 2:
+    console.log(iLong + " километров = " + iLong * 1000 + " метров");
+    break;
+    case 3:
+    console.log(iLong + " метров = " + iLong + " метров");
+    break;
+    case 4:
+    console.log(iLong + " миллиметр = " + iLong / 1000 + " метров");
+    break;
+    case 5:
+    console.log(iLong + " сантиметров = " + iLong / 100 + " метров");
+    break;
+}
+
+console.log("Задача №3.Дано целое число, лежащее в диапазоне от -999 до 999. Вывести строку - словесное описание данного числа вида отрицательное двузначное число, нулевое число, положительное однозначное число и т.д.");
+var number = 42;
+if(number < 0 && String(number).length == 3 && String(number).charAt(0) == "-"){
+    console.log("Число " + number + " - " + "отрицательное двухзначное число");
+}else if(number > 0 && String(number).length == 1){
+    console.log("Число " + number + " - " + "положительное однозначное число");
+}else if(number == 0 ){
+    console.log("Число " + number + " - " + "нулевое");
+}else {
+    console.log("Число " + number + " - " + "не удовлетворяет условию задачи");
+}
