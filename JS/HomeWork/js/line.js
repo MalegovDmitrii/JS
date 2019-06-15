448d7e3a0897913a0658e69bd375dd0be635076d
MalegovDmitrii <d-malegov@mail.ru>
2019-06-15 13:17:41 +0300

    sdsdszxczxc

--- JS/HomeWork/js/line.js
+++ JS/HomeWork/js/line.js
@@ -67,13 +67,14 @@ function deleteElement()
     var num = 0;
     for (var line = 2; line < 26; line++) {
         var element = document.getElementById('cell' + line);
-        if(document.getElementById('cell' + line) == document.getElementById('cell' + line - 1))
+        if(document.getElementById('cell' + line).style.background ==
+        document.getElementById('cell' + (line - 1)).style.background)
         {
             num++
         } else {
             if(num >= 3){
                     for (var i = 0; i < num; i++) {
-                    document.getElementById('cell' + line-i).style.background =
+                    document.getElementById('cell' + (line-i)).style.background =
                                getRandomNum(1, 5) == 1 ? 'red' :
                               (getRandomNum(1, 5) == 2 ? 'blue' :
                               (getRandomNum(1, 5) == 3 ? 'green' :
